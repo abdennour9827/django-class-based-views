98d505bd4a366f262db18e960aa5bee06fc66c16
unknown <abde.nour9827@gmail.com>
2022-11-21 11:41:06 +0100

    add class based view structur

--- post/views.py
+++ post/views.py
@@ -4,7 +4,14 @@ from .models import Post
 # Create your views here.
 
 class PostList(ListView):
-    model = Post
+    model = Post    # in template object_list or {nameModel}_list
+    #context_object_name = 'all_posts'  # ila bghiti tbdl smya bach katrd 
+    #ordering = ['-created_at']
+    queryset = Post.objects.filter(active=False)
+
+
+
+
 
 class PostDetail(DetailView):
     model = Post
